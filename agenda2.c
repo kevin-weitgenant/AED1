83e05e6a96086cc055f2fdaf7f66ad84298adac7
kevin-weitgenant <kevin.weitgenant@gmail.com>
2019-05-13 22:20:02 -0300

    buscar
    falta terminar de codar a função deletar

--- agenda2.c
+++ agenda2.c
@@ -56,6 +56,7 @@ void adiciona(){
 void listar(){
 
     reapontar();
+
     frase = (char*)paluno;
 
     for(Comeco->i = 0;( Comeco->i) < (Comeco->qtd); (Comeco->i)++){
@@ -70,23 +71,37 @@ void listar(){
 
 }
 
-/*
+
 void buscar(){
-    if (*p == 1){
-        for(*i = 0; *i<*qtd; (*i)++){
-            reapontar();
-            paluno = sizeof(char)*20 + paluno;
-            paluno = paluno + (*i) * sizeof(aluno);
-            printf("*i = %d paluno->nome = %s\n\n\n", *i,(paluno->nome));
-            if (strcmp((paluno->nome),frase) == 0){
-                printf("Nome: %s\n", paluno->nome);
+    reapontar();
+    frase = (char*)paluno;
+
+    if (Comeco->p == 1){
+        for(Comeco->i = 0;( Comeco->i) < (Comeco->qtd); (Comeco->i)++){
+            paluno = (aluno*)(frase + (sizeof(char)*(strlen(frase)+1)));
+            if (strcmp(Comeco->comparar,frase) == 0){
+                printf("Nome: %s\n", frase);
                 printf("Idade: %d\n",paluno->idade);
                 printf("Matricula: %d\n\n\n",paluno->matricula);
+
             }
+            frase = (char*) (paluno +1);
         }
     }
 }
-*/
+
+void deletar(){
+    reapontar();
+    
+
+
+
+}
+
+
+
+
+
 
 int main()
 {
@@ -108,30 +123,43 @@ int main()
             listar();
 
         }
-    }
-    return 0;}
 
-        /*else if (*p == 3){
-            while (*p != 4){
+
+        //BUSCAR
+        else if (Comeco->p == 3){
+            while (Comeco->p != 4){
                 printf("Escolha como deseja buscar\n(1) Nome\n(2) Idade\n(3) Matricula\n(4) Voltar para o menu\n");
-                scanf("%d", p);
-                if (*p == 1){
+                scanf("%d", &Comeco->p);
+                if (Comeco->p == 1){
                     printf("Digite o nome a ser procurado: ");
-                    scanf("%s", frase);
+                    scanf("%s", Comeco->comparar);
                     buscar();
                 }
-                else if (*p ==2){
+                else if (Comeco->p ==2){
                     printf("Digite a idade a ser buscada: ");
                     scanf("%s", frase);
                     buscar();
                 }
-                else if (*p ==3){
+                else if (Comeco->p ==3){
                     printf("Digite a matricula a ser procurada: ");
                     scanf("%s", frase);
                     buscar();
-                else  printf("Invalido... Tente novamente\n\n");
-            }
+                }
+                else if (Comeco->p != 4)  printf("Invalido... Tente novamente\n\n");
+                }
         }
+        
+        //DELETAR
+        else if (Comeco->p == 4){
+                printf("Digite o nome a ser deletado, :\n");
+                scanf("%s", Comeco->comparar);
+                deletar();
+                listar();
+        }
+    
+    
     }
+
+
     return 0;
-}*/
+}
